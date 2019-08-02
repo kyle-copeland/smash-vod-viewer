8af8a21e327f6e827329756f83e0819183e95744
Kyle Copeland <copelandky@gmail.com>
2019-08-01 22:41:31 -0700

    feat: add static storybook folder

---
+++ docs/main.e23fe2e1d1ee7bf9f091.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,p){p(349),p(451),n.exports=p(621)},451:function(n,o,p){"use strict";p.r(o);p(452),p(591),p(594)}},[[0,1,2]]]);
---
+++ docs/precache-manifest.e8d734622e31026915e831095f2be56b.js
@@ -0,0 +1,55 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35993e614ddc8ea2613a4e1b6bee6273",
+    "url": "../build/lib/App.d.ts"
+  },
+  {
+    "revision": "e2ebd7ddedcadeeadbf819c35985c768",
+    "url": "../build/lib/App.test.d.ts"
+  },
+  {
+    "revision": "dce15b629ae6fb3d7786e1f38dee9335",
+    "url": "../build/lib/Button/index.d.ts"
+  },
+  {
+    "revision": "e2ebd7ddedcadeeadbf819c35985c768",
+    "url": "../build/lib/Button/index.stories.d.ts"
+  },
+  {
+    "revision": "2f1ad574773c9c22dd10079d71ba2e50",
+    "url": "../build/lib/Sidebar/index.d.ts"
+  },
+  {
+    "revision": "e2ebd7ddedcadeeadbf819c35985c768",
+    "url": "../build/lib/Sidebar/index.stories.d.ts"
+  },
+  {
+    "revision": "e2ebd7ddedcadeeadbf819c35985c768",
+    "url": "../build/lib/colors.stories.d.ts"
+  },
+  {
+    "revision": "780b296fad9ac72fe7590acb6f2e395b",
+    "url": "../build/lib/index.d.ts"
+  },
+  {
+    "revision": "9b4b4c2507b786d0ea5fb76d3c2bccf9",
+    "url": "../build/lib/serviceWorker.d.ts"
+  },
+  {
+    "revision": "83aa56f6b74cb7e10119c68306378810",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "8496bbd45b3b2f7f2e61",
+    "url": "main.css"
+  },
+  {
+    "url": "main.d67f3d9b7f7dfc751a69.bundle.js"
+  },
+  {
+    "url": "runtime~main.d67f3d9b7f7dfc751a69.bundle.js"
+  },
+  {
+    "url": "vendors~main.d67f3d9b7f7dfc751a69.bundle.js"
+  }
+]);
